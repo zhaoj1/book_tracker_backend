53765c1eb2feb4feac6d49ff42eaa4e90dbb2f80
github username <justinzhao265@gmail.com>
2020-06-08 17:56:52 -0400

    login completed

--- book_tracker_api/urls.py
+++ book_tracker_api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework_jwt.views import obtain_jwt_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
+    path('token-auth/', obtain_jwt_token),
+    path('core/', include('core.urls')),
 ]
