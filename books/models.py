26c13fecee4a46e6a8d6ae21390b9b423531d208
github username <justinzhao265@gmail.com>
2020-06-16 16:53:59 -0400

    book models adjustments

--- books/models.py
+++ books/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from jsonfield import JSONField
 
 class Book(models.Model):
-  title = models.CharField(max_length=200)
-  author = models.CharField(max_length=200)
-  description = models.CharField(max_length=200)
-  owner = models.CharField(max_length=5)
+  # title = models.CharField(max_length=200)
+  # author = models.CharField(max_length=200)
+  # description = models.CharField(max_length=200)
+  # owner = models.CharField(max_length=5)
+  json = JSONField()
+  owner = models.IntegerField()
 
   def __str__(self):
     return self.title
--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 from django.contrib.auth.models import User
-
+from jsonfield import JSONField
 
 class UserSerializer(serializers.ModelSerializer):
 
