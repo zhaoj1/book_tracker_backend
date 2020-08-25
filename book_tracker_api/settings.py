5c2aebbde609aa466e9c596db725633b8923475b
github username <justinzhao265@gmail.com>
2020-08-25 19:51:03 -0400

    heroku

--- book_tracker_api/settings.py
+++ book_tracker_api/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('KEY')
+# SECRET_KEY = config('KEY')
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
