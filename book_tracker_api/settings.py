100a0b9c54a4896245a9c33a375f1bd04e217ccf
github username <justinzhao265@gmail.com>
2020-08-25 19:02:26 -0400

    heroku setup

--- book_tracker_api/settings.py
+++ book_tracker_api/settings.py
@@ -134,10 +134,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# import django_heroku
+# django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
-
-import django_heroku
-django_heroku.settings(locals())
